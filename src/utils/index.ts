363e1ed9be43ea30afc2a0ba3923588b524f27f9
Utkarsh <utkarsh1@appinventiv.com>
2022-09-05 13:05:36 +0530

    update eslint command and ignore-file rules

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,27 +1,20 @@
-
 /**
  * To validate email
  * @param email email string to validate
  * @returns  return true or false
  */
-export const validateEmail = (email: string) => {
-    return /[a-z0-9]+@[a-z]{2,}\.[a-z]{2,3}/.test(email);
-  };
-  
+export const validateEmail = (email: string) => /[a-z0-9]+@[a-z]{2,}\.[a-z]{2,3}/.test(email);
+
 /**
  * To validate phone
  * @param phone  phone string to validate
  * @returns return true or false
  */
-export const validatePhone = (phone:string)=>{
-   return  (/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/).test(phone);
-}
+export const validatePhone = (phone:string) => (/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/).test(phone);
 
 /**
  * To validate password is strong or not
  * @param password password string to validate
  * @returns return true or false
  */
-export const validatePassword = (password: string) => {
-    return /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/.test(password);
-  };
+export const validatePassword = (password: string) => /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/.test(password);
--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -2,14 +2,12 @@
  * To get token value from the storage
  * @returns return token
  */
-export const getAccessToken = ()=>{
-    return localStorage.getItem('token')
-}
+export const getAccessToken = () => localStorage.getItem('token');
 
 /**
  * To set token value in the storage
  * @param token token value to store
  */
-export const setAccessToken = (token:string)=>{
-  localStorage.setItem('token',token)
-}
+export const setAccessToken = (token:string) => {
+  localStorage.setItem('token', token);
+};
